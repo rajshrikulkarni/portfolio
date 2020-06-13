8b47d48145706ba76aa167fc8bea64b37bdcc5de
rajkulkarni197 <46705911+rajkulkarni197@users.noreply.github.com>
2020-06-13 19:52:10 +0530

    ok

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0c233063b2e54a8b0dc9b92e0d09da0e.js"
+  "/portfolio/precache-manifest.772ca4f79718e90e69ce6eb5531908d3.js"
 );
 
 self.addEventListener('message', (event) => {
